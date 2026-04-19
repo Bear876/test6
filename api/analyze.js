1c0210b66feff1e7a880de465b99ea6413f025cd
Kanav <kanavsah@gmail.com>
2026-04-19 18:46:21 -0500

    Add files via upload

--- api/analyze.js
+++ api/analyze.js
@@ -22,7 +22,7 @@ export default async function handler(req, res) {
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
           contents: [{ parts: [{ inline_data: { mime_type: mediaType, data: base64 } }, { text: prompt }] }],
-          generationConfig: { temperature: 0.05, maxOutputTokens: 4000 }
+          generationConfig: { temperature: 0.05, maxOutputTokens: 8192 }
         })
       }
     );
